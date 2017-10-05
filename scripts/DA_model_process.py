e33d52ec5be85d69c6a0fe8b18816b5e4b31d2e0
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-05 16:40:40 +0200

    Model process

--- scripts/DA_model_process.py
+++ scripts/DA_model_process.py
@@ -1,29 +1,42 @@
 from sklearn.ensemble import  RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import  RandomForestClassifier
+
 import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 
 import sys
 
 sys.path.append('..')
-from scripts.kaggle_solver import KaggleSolver
 import pandas as pd
+
+
+def extract(df):
+    cat_columns = ['sponsor', 'grant_category']
+    for cat_column in cat_columns:
+        df[cat_column] = df[cat_column].astype('category')
+        df[cat_column] = df[cat_column].cat.codes
+    df = df.drop('date', axis=1)
+    relevant_columns = [x for x in df.columns if x not in ['id', 'granted']]
+    X = df[relevant_columns]
+    y = df['granted']
+    return X, y
+
 
 if __name__ == "__main__":
 
     df_train = pd.read_csv('../data/train_apps_rfcd_seo_mod.csv', parse_dates=True)
+    df_test = pd.read_csv('../data/test_apps_rfcd_seo_mod.csv', parse_dates=True)
 
-    cat_columns = ['sponsor', 'grant_category', 'sponsor_c', 'grant_category_c']
-    for cat_column in cat_columns:
-        df_train[cat_column] = df_train[cat_column].astype('category')
-        df_train[cat_column] = df_train[cat_column].cat.codes
-    df_train = df_train.drop('date', axis=1)
+    X_train, y_train = extract(df_train)
+    X_test, y_test= extract(df_test)
+    print(X_train)
+    print(y_train)
 
-    relevant_columns = [x for x in df_train.columns if x not in ['id', 'granted']]
-
-    X_train = df_train[relevant_columns]
-    y_train = df_train['granted']
-
-    classifier = RandomForestRegressor()
+    classifier = RandomForestClassifier( n_estimators=2000)
     classifier.fit(X=X_train, y=y_train)
 
+    y_pred = classifier.predict(X_test)
 
-
+    print(accuracy_score(y_test, y_pred))
+    print(roc_auc_score(y_test, y_pred))
