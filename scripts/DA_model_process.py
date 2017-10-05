1713bcf68a5e71dea78fbe8564082e0635af5c09
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-05 17:21:54 +0200

    XGBoost added

--- scripts/DA_model_process.py
+++ scripts/DA_model_process.py
@@ -1,10 +1,10 @@
 from sklearn.ensemble import  RandomForestRegressor, GradientBoostingRegressor
 from sklearn.ensemble import  RandomForestClassifier
-
-import numpy as np
+from xgboost import XGBClassifier, XGBRegressor
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 import sys
 
 sys.path.append('..')
@@ -19,24 +19,49 @@ def extract(df):
     df = df.drop('date', axis=1)
     relevant_columns = [x for x in df.columns if x not in ['id', 'granted']]
     X = df[relevant_columns]
-    y = df['granted']
+    y = df[['granted']]
     return X, y
 
+def get_random_classifier():
+    return RandomForestClassifier(n_estimators=2000)
+
+def get_random_regressor():
+    return RandomForestRegressor(n_estimators=2000)
+
+
+def get_regressor_xgboost():
+    return XGBRegressor(max_depth=3, min_child_weight=1)
+
+
+def get_regressor_xgboost2():
+    return XGBRegressor(max_depth=5, min_child_weight=3)
+
+
+def print_best_parameters(classifier):
+    if hasattr(classifier, "best_estimator_"):
+        best_parameters = classifier.best_estimator_.get_params()
+        for param_name in sorted(best_parameters.keys()):
+            print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
+
+def test_with_classif(classifier, df_train, df_test):
+    print(classifier)
+    X_train, y_train = extract(df_train)
+    X_test, y_test = extract(df_test)
+
+    classifier.fit(X=X_train, y=y_train)
+    y_pred = classifier.predict(X_test)
+    # print(accuracy_score(y_test, y_pred))
+    print_best_parameters(classifier)
+    print(roc_auc_score(y_test, y_pred))
+    scores = cross_val_score(classifier, X_train, y_train, cv=5)
+    print(scores)
 
 if __name__ == "__main__":
 
     df_train = pd.read_csv('../data/train_apps_rfcd_seo_mod.csv', parse_dates=True)
     df_test = pd.read_csv('../data/test_apps_rfcd_seo_mod.csv', parse_dates=True)
 
-    X_train, y_train = extract(df_train)
-    X_test, y_test= extract(df_test)
-    print(X_train)
-    print(y_train)
+    test_with_classif(get_regressor_xgboost2(), df_train, df_test)
 
-    classifier = RandomForestClassifier( n_estimators=2000)
-    classifier.fit(X=X_train, y=y_train)
-
-    y_pred = classifier.predict(X_test)
-
-    print(accuracy_score(y_test, y_pred))
-    print(roc_auc_score(y_test, y_pred))
+    test_with_classif(get_regressor_xgboost(),df_train, df_test)
