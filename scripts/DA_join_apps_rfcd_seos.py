5270e1a129104b25ae2034dbb43fa6032edf5c68
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-05 13:47:17 +0200

    New script to join stuff

---
+++ scripts/DA_join_apps_rfcd_seos.py
@@ -0,0 +1,15 @@
+import pandas as pd
+seo_train = pd.read_csv('../data/train_seo_mod.csv')
+rfcd_train = pd.read_csv('../data/train_rfcd_mod.csv')
+app_train = pd.read_csv('../data/train_apps_mod.csv')
+
+app_rfcd_train = app_train.merge(rfcd_train, on='id', how='outer')
+
+rfcd_train_pivoted = rfcd_train.pivot( index='id', columns='RFCD.Code', values='RFCD.Percentage').fillna(0)
+seo_train_pivoted  = seo_train.pivot( index='id', columns='SEO.Code', values='SEO.Percentage').fillna(0)
+rfcd_train_pivoted.rename(columns=lambda x: "RFCD_"+str(x), inplace=True)
+seo_train_pivoted.rename(columns=lambda x: "SEO_"+str(x), inplace=True)
+app_rfcd_train = app_train.join(rfcd_train_pivoted, how='outer').fillna(0)
+
+app_rfcd_seo_train = app_rfcd_train.join(seo_train_pivoted, how='outer').fillna(0)
+app_rfcd_seo_train.to_csv('../data/train_apps_rfcd_seo_mod.csv')
