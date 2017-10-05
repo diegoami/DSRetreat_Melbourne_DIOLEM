916afc58b0fd97c7f43716e7859e484adf51e0f1
nelai1 <olselig@gmail.com>
2017-10-05 14:51:12 +0200

    added lookup tables for probabilies in apps

--- scripts/o_create_apps_raw.py
+++ scripts/o_create_apps_raw.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pandas as pd
 import os.path
-
+import json
 
 def create_apps(df):
     apps = df.iloc[:, :6]
@@ -32,16 +32,45 @@ def replace_grant_values(apps):
     apps['grant_value'].replace(rep, inplace=True)
 
 
-def process(dataset = 'train'):
+def gen_dist(apps):
+    dists = {}
+
+    keys = ['grant_value', 'sponsor', 'grant_category']
+    for k in keys:
+        values_agg = apps[k].value_counts().sort_index()
+        values = values_agg.index
+        prob = values_agg.values
+        prob = prob / int(prob.sum())
+        # print(prob.sum())
+        dists[k]= {'probability':prob.tolist() ,'values':values.tolist()}
+    return dists
+
+
+def gen_lists(apps):
+    category_list = apps['grant_category'].value_counts().index
+    temp = apps['sponsor'].value_counts()
+    sponsor_list = temp[temp>5]
+    return category_list.tolist(), sponsor_list.index.tolist()
+
+
+def process(dataset = 'train', createDist = True):
     base = '../data/'
     readfile = dataset+'.csv'
     writefile = dataset + '_apps_raw.csv'
+    distfile = 'apps_dist_file.json'
 
     df = pd.read_csv(os.path.join(base, readfile), low_memory=False)
     apps = create_apps(df)
     apps.to_csv(os.path.join(base, writefile), index=False)
     print('File {} generated.'.format(writefile))
 
+    if createDist:
+        dist = gen_dist(apps)
+        category_list, sponsor_list = gen_lists(apps)
+        dist['lists'] = {'category':category_list,'sponsor':sponsor_list}
+        with open(os.path.join(base,distfile),'w') as fp:
+            json.dump(dist, fp)
+        print('dist values written to {}'.format(distfile))
 
 if __name__ == '__main__':
     process()
--- scripts/o_process_apps.py
+++ scripts/o_process_apps.py
@@ -1,45 +1,60 @@
 import numpy as np
 import pandas as pd
 import os.path
+import json
+
+# def split_sponsor_and_grant_category(apps):
+#     apps['sponsor_c'] = apps['sponsor'].str.extract('([A-Z])', expand=False)
+#     apps['sponsor_n'] = apps['sponsor'].str.extract('(\d+)', expand=False)
+#     apps['grant_category_c'] = apps['grant_category'].str.extract('([A-Z])', expand=False)
+#     apps['grant_category_n'] = apps['grant_category'].str.extract('(\d+)', expand=False)
 
 
-def split_sponsor_and_grant_category(apps):
-    apps['sponsor_c'] = apps['sponsor'].str.extract('([A-Z])', expand=False)
-    apps['sponsor_n'] = apps['sponsor'].str.extract('(\d+)', expand=False)
-    apps['grant_category_c'] = apps['grant_category'].str.extract('([A-Z])', expand=False)
-    apps['grant_category_n'] = apps['grant_category'].str.extract('(\d+)', expand=False)
+# def fill_nan_by_key(apps, key):
+#     values_agg = apps[key].value_counts().sort_index()
+#     values = values_agg.index
+#     prob = values_agg.values
+#     prob = prob / int(prob.sum())
+#     mask = apps[key].isnull()
+#     rands = np.random.choice(values, mask.sum(), p=prob)
+#     apps.loc[mask, key] = rands
 
 
-def fill_nan_by_key(apps, key):
-    values_agg = apps[key].value_counts().sort_index()
-    values = values_agg.index
-    prob = values_agg.values
-    prob = prob / int(prob.sum())
-    mask = apps[key].isnull()
-    rands = np.random.choice(values, mask.sum(), p=prob)
-    apps.loc[mask, key] = rands
-
-
-def fill_nans(apps):
+def fill_nans(apps, dist):
     keys = ['grant_value', 'sponsor', 'grant_category']
     for k in keys:
-        fill_nan_by_key(apps, k)
+        prob = dist[k]['probability']
+        values = dist[k]['values']
+
+        mask = apps[k].isnull()
+        rands = np.random.choice(values, mask.sum(), p=prob)
+        apps.loc[mask, k] = rands
 
 
-def rebin_values(apps):
+def rebin_values(apps,dist):
+    #grant values
     mask = apps['grant_value'] > 400001
     apps.loc[mask, 'grant_value'] = 500000
+    #sponsor
+    mask = apps['sponsor'].isin(dist['lists']['sponsor'])
+    apps.loc[~mask,'sponsor'] = 'other'
+    mask = apps['grant_category'].isin(dist['lists']['category'])
+    apps.loc[~mask, 'grant_category'] = 'other'
 
 
 def process(dataset = 'train'):
     base = '../data/'
     readfile = dataset + '_apps_raw.csv'
     writefile = dataset + '_apps_mod.csv'
+    distfile = 'apps_dist_file.json'
+
+    with open(os.path.join(base, distfile), 'r') as fp:
+        dist = json.load(fp) #yield dist
 
     apps = pd.read_csv(os.path.join(base, readfile), low_memory=False)
-    fill_nans(apps)
-    split_sponsor_and_grant_category(apps)
-    rebin_values(apps)
+    fill_nans(apps,dist)
+    # split_sponsor_and_grant_category(apps)
+    rebin_values(apps,dist)
     apps.to_csv(os.path.join(base, writefile), index=False)
     print('File {} generated.'.format(writefile))
 
