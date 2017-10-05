97c40782b4204ce364de046d79443b83734a02a5
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-05 14:38:15 +0200

    Updated with index, and column other

--- scripts/DA_join_apps_rfcd_seos.py
+++ scripts/DA_join_apps_rfcd_seos.py
@@ -12,4 +12,8 @@ seo_train_pivoted.rename(columns=lambda x: "SEO_"+str(x), inplace=True)
 app_rfcd_train = app_train.join(rfcd_train_pivoted, how='outer').fillna(0)
 
 app_rfcd_seo_train = app_rfcd_train.join(seo_train_pivoted, how='outer').fillna(0)
+
+app_rfcd_seo_train['RFCD_OTHER'] = 100-app_rfcd_seo_train[[x for x in app_rfcd_seo_train.columns if x.startswith('RFCD') ]].sum(axis=1)
+app_rfcd_seo_train['SEO_OTHER'] = 100-app_rfcd_seo_train[[x for x in app_rfcd_seo_train.columns if x.startswith('SEO') ]].sum(axis=1)
+app_rfcd_seo_train.set_index('id', inplace=True)
 app_rfcd_seo_train.to_csv('../data/train_apps_rfcd_seo_mod.csv')
