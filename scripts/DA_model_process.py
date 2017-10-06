1d0e00f4a38be79f235251d2093fb6da4a087756
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-06 10:55:09 +0200

    seperated extract from process in the pipeline

--- scripts/DA_model_process.py
+++ scripts/DA_model_process.py
@@ -5,50 +5,14 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
+from scripts.DA_model_extract import extract
 import sys
 
 sys.path.append('..')
 import pandas as pd
 
 
-def extract(df_train, df_test):
-    cat_columns = ['sponsor', 'grant_category']
-    for cat_column in cat_columns:
-        df_train[cat_column] = df_train[cat_column].astype('category')
-        df_train[cat_column] = df_train[cat_column].cat.codes
-        df_test[cat_column] = df_test[cat_column].astype('category')
-        df_test[cat_column] = df_test[cat_column].cat.codes
 
-
-    df_train = df_train.drop('date', axis=1)
-    df_test = df_test.drop('date', axis=1)
-
-    df_train.fillna(0, inplace=True)
-    df_test.fillna(0, inplace=True)
-
-    relevant_columns = [x for x in set(list(df_train.columns)+list(df_test.columns)) if x not in ['id', 'granted']]
-    #print(relevant_columns)
-    missing_train_columns = [x for x in relevant_columns if x not in df_train ]
-
-
-    missing_test_columns = [x for x in relevant_columns if x not in df_test ]
-    print(missing_test_columns)
-
-    df_train.reindex(columns=list(df_train.columns)+missing_train_columns)
-
-    df_test = df_test.reindex(columns=(list(df_test.columns) + missing_test_columns))
-
-    X_train = df_train[relevant_columns]
-    y_train = df_train['granted']
-    X_test = df_test[relevant_columns]
-    y_test = df_test['granted']
-
-    y_train.fillna(0, inplace=True)
-    y_test.fillna(0, inplace=True)
-    X_train.fillna(0, inplace=True)
-    X_test.fillna(0, inplace=True)
-
-    return X_train, y_train, X_test, y_test
 
 def get_random_classifier():
     return RandomForestClassifier(n_estimators=200)
